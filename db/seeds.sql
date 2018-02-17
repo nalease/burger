9fff5637d70d42d519a5bdecc1c8ebc7b3379772
Nalease Marrow <Naleasem@Naleases-MacBook-Pro.local>
2018-02-17 08:16:54 -0500

    update server

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES  ('Big Mac', false, CURRENT_TIMESTAMP),
+('Mac&Cheese Stack', false, CURRENT_TIMESTAMP), 
+('Baconator', false, CURRENT_TIMESTAMP)
--- server.js
+++ server.js
@@ -3,3 +3,34 @@ var express = require("express");
 var methodOR = require("method-override");
 var bodyParser = require("body-parser");
 
+var app = express();
+var port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "July_amis08",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(port);
